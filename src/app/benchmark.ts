b72c39d046489cc23b546472508f2dd9fc106844
Filipe Duarte <fmmendo@live.com>
2018-01-21 11:38:16 +0000

    better data model

--- src/app/benchmark.ts
+++ src/app/benchmark.ts
@@ -1,12 +1,21 @@
 export class Benchmark {
     id: number;
     name: string;
+    standards: string;
+    level: number[];
+    modifier: string[];
+    scheme: string;
+    category: Category;
+}
 
-    level: string;
+export enum Category {
+    Gymnastics,
+    Conditioning,
+    Weightlifting
 }
 
 export const BENCHMARKS: Benchmark[] = [
-    { id: 11, name: 'Pull-ups', level: '1' },
-    { id: 12, name: 'Push-ups', level: '1' },
-    { id: 13, name: 'Sit-ups', level: '1' }
+    { id: 11, name: 'Pull-ups', standards: 'some standards', level: [1,2,3], modifier: ['a', 'b', 'c'], scheme:'dunno', category: Category.Gymnastics },
+    { id: 12, name: 'Push-ups', standards: 'some standards', level: [1,2,3], modifier: ['a', 'b', 'c'], scheme:'dunno', category: Category.Gymnastics },
+    { id: 13, name: 'Sit-ups', standards: 'some standards', level: [1,2,3], modifier: ['a', 'b', 'c'], scheme:'dunno', category: Category.Gymnastics }
 ]
